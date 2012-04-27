a3098eacb1a2acb06dd031108ca8b4150ef84636
francispereira <francispereira@7terminals.com>
2012-04-27 13:17:14 +0530

    created structure for module

---
+++ generic/manifests/user.pp
@@ -0,0 +1,34 @@
+# Class: puppet-generic::user
+#
+# This module manages puppet-generic::user
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class puppet-generic::user ($allowdupe,
+	$comment,
+	$ensure,
+	$expiry,
+	$gid,
+	$groups,
+	$home,
+	$managehome,
+	$name,
+	$password,
+	$password_max_age,
+	$password_min_age,
+	$profiles,
+	$project,
+	$shell,
+	$system,
+	$uid) {
+	user { $name:
+		
+	}
+}
