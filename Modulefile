e35a704af35965ac6242964a964dc78e23d284b3
francispereira <francispereira@7terminals.com>
2012-04-27 11:12:15 +0530

    initial commit

---
+++ Modulefile
@@ -0,0 +1,9 @@
+name 'Francis-puppet-generic'
+version '0.01'
+
+author 'Francis'
+license ''
+project_page 'https://github.com/francispereira/puppet-generic/wiki'
+source 'https://github.com/francispereira/puppet-generic'
+summary ''
+description ''
---
+++ manifests/init.pp
@@ -0,0 +1,17 @@
+# Class: puppet-generic
+#
+# This module manages puppet-generic
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class puppet-generic {
+
+
+}
