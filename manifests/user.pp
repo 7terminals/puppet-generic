42ef1074ff1b4f4fddef37778631d9c0b7ecee6f
francispereira <francispereira@7terminals.com>
2012-04-30 18:54:27 +0530

    aligned documentation to 80 characters

--- manifests/user.pp
+++ manifests/user.pp
@@ -5,16 +5,21 @@
 # Parameters:
 #	comment					- 	A description for the user
 #	ensure					- 	present or absent. Rresent by default
-#	gid						- 	Primary gid for the user. Should be numeric. Will be automatically created
+#	gid						- 	Primary gid for the user. Should be numeric. 
+#								Will be automatically created
 #	groups 					-	Additional groups that the user belongs to
 #	home					-	The home directory for the user
-#	managehome 				-	Whether to manage the home directory when managing the user. Defaults to false. 
+#	managehome 				-	Whether to manage the home directory when 
+#								managing the user. Defaults to false. 
 #								Valid values are true, false
-#	password				- 	The user’s password, in whatever encrypted format the local system requires
+#	password				- 	The user’s password, in whatever encrypted 
+#								format the local system requires
 #	shell 					- 	The user's login shell
-#	system					-	Whether the user is a system user, according to the OS’s criteria
+#	system					-	Whether the user is a system user, according to 
+#								the OS’s criteria
 #	uid						- 	The user ID; must be specified numerically.
-#	ssh_authorized_key_type	-	User's public key type. Can be ssh-rsa or ssh-dsa. Defaults to ssh-rsa
+#	ssh_authorized_key_type	-	User's public key type. Can be ssh-rsa or 
+#								ssh-dsa. Defaults to ssh-rsa
 #
 # Actions:
 # 	Creates a user, group and/or ssh_authorised_key for the user
