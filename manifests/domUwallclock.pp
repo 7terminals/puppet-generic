9c71a443e6105e847b6473c5c656b713ac43d6d6
francispereira <francispereira@7terminals.com>
2012-10-23 13:12:46 +0530

    initial work for independent wallclock for xen domU

---
+++ manifests/domUwallclock.pp
@@ -0,0 +1,20 @@
+# Define: generic::domUwallclock
+#
+# This file provides definition generic::domUwallclock
+define generic::domUwallclock {
+	if ($::virtual == 'xenu') {
+		file {
+			'/proc/sys/xen/independent_wallclock' :
+				content => '1',
+		}
+		exec {
+			'echo "independent_wallclock=1" >> /etc/sysctl.conf' :
+				unless => 'grep -q independent_wallclock=1 /etc/sysctl.conf '
+		}
+	}
+	else {
+		notify {
+			'Not a Xen domU.':
+		}
+	}
+} 
